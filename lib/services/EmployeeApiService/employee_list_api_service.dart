61f789f016d0dd027644e7d1e018b205ee44b9f2
Sneha S <sneha.s@Amishas-Mac.local>
2022-04-07 15:04:13 +0530

    make Employee nullable

--- lib/services/EmployeeApiService/employee_list_api_service.dart
+++ lib/services/EmployeeApiService/employee_list_api_service.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:projectunity/model/Employee/employee.dart';
-import 'package:projectunity/rest/data_exception.dart';
 import 'package:projectunity/user/user_preference.dart';
 import 'package:projectunity/utils/constant.dart';
 import 'package:projectunity/utils/data_exception.dart';
