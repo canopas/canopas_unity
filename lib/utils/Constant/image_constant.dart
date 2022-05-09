a0f1ba9ee7549a5d792c5a4e770bd3fc4b0df488
sneha.s <sneha.s@canopas.com>
2022-05-09 16:57:45 +0530

    Row design for onboard screen

--- lib/utils/Constant/image_constant.dart
+++ lib/utils/Constant/image_constant.dart
@@ -2,3 +2,4 @@ const String loginPageBackgroundImage =
     'assets/images/login_background_image.jpg';
 const String loginPageVectorImage = 'assets/images/login_page_image.jpg';
 const String googleLogoImage = 'assets/images/google_logo.png';
+const String onBoardScreen1Image = 'assets/images/onboard-screen1-image.jpg';
