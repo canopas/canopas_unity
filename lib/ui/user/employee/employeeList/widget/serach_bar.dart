ea035cb285793dc2a4ece3afae005d32bb6d28fe
Sneha Canopas <sneha.s@canopas.com>
2022-06-06 05:01:14 +0000

    Fix employee list screen ui

--- lib/ui/user/employee/employeeList/widget/serach_bar.dart
+++ lib/ui/user/employee/employeeList/widget/serach_bar.dart
@@ -10,30 +10,38 @@ class SearchBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
     return Container(
       margin: const EdgeInsets.only(top: 30, right: 30, left: 30),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       height: 45,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(20), color: Colors.grey.shade200),
-      child: Padding(
-        padding: const EdgeInsets.only(left: 10),
-        child: TextField(
-          cursorColor: Colors.black54,
-          style: GoogleFonts.ibmPlexSans(fontSize: 20, color: Colors.black87),
-          decoration: InputDecoration(
-              border: InputBorder.none,
-              prefixIcon: const Icon(
-                Icons.search,
-                color: Colors.grey,
-              ),
-              hintText: 'Search your Colleagues by name',
-              hintStyle: height >= 700
-                  ? const TextStyle(fontSize: 20)
-                  : const TextStyle(fontSize: 16)),
-          textAlign: TextAlign.start,
-          keyboardType: TextInputType.text,
-        ),
+      decoration: ShapeDecoration(
+          shape: const StadiumBorder(), color: Colors.grey.shade200),
+      child: Row(
+        children: [
+          const Icon(
+            Icons.search,
+            color: Colors.grey,
+            size: 26,
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: TextField(
+              cursorColor: Colors.black54,
+              style:
+                  GoogleFonts.ibmPlexSans(fontSize: 20, color: Colors.black87),
+              decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  contentPadding: EdgeInsets.only(bottom: 13),
+                  hintText: 'Search your Colleagues by name',
+                  hintStyle: TextStyle(fontSize: 17)),
+              textAlign: TextAlign.start,
+              keyboardType: TextInputType.text,
+              textAlignVertical: TextAlignVertical.center,
+            ),
+          ),
+        ],
       ),
     );
   }
