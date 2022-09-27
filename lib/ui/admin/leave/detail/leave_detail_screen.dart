2e62867749b1d2b918798b5d8e72e221f4172693
Radhika Canopas <radhika.s@canopas.com>
2022-09-27 12:32:31 +0530

    Remove unnecessary widget, and white background from buttons on leave detail screen

--- lib/ui/admin/leave/detail/leave_detail_screen.dart
+++ lib/ui/admin/leave/detail/leave_detail_screen.dart
@@ -1,29 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:projectunity/bloc/admin/leave/leave_application_bloc.dart';
 import 'package:projectunity/configs/text_style.dart';
 import 'package:projectunity/core/extensions/date_time.dart';
+import 'package:projectunity/di/service_locator.dart';
 import 'package:projectunity/model/leave_application.dart';
-import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:projectunity/navigation/navigation_stack_manager.dart';
+import 'package:projectunity/stateManager/admin/leave_status_manager.dart';
+import 'package:projectunity/ui/admin/leave/detail/widget/user_content.dart';
 import 'package:projectunity/widget/Leave_details_screen_widgets/leave_details_header_content.dart';
 import 'package:projectunity/widget/Leave_details_screen_widgets/reason_content.dart';
 import 'package:projectunity/widget/Leave_details_screen_widgets/remaining_leave_content.dart';
-import 'package:projectunity/ui/admin/leave/detail/widget/user_content.dart';
+
 import '../../../../configs/colors.dart';
 import '../../../../core/utils/const/other_constant.dart';
 import '../../../../model/leave/leave.dart';
-import 'widget/button_content.dart';
 
 class AdminLeaveRequestDetailScreen extends StatefulWidget {
   final LeaveApplication employeeLeave;
 
-   const AdminLeaveRequestDetailScreen({Key? key, required this.employeeLeave})
+  const AdminLeaveRequestDetailScreen({Key? key, required this.employeeLeave})
       : super(key: key);
 
   @override
-  State<AdminLeaveRequestDetailScreen> createState() => _AdminLeaveRequestDetailScreenState();
+  State<AdminLeaveRequestDetailScreen> createState() =>
+      _AdminLeaveRequestDetailScreenState();
 }
 
-class _AdminLeaveRequestDetailScreenState extends State<AdminLeaveRequestDetailScreen> {
-  final TextEditingController _approvalOrRejectionMassage = TextEditingController();
+class _AdminLeaveRequestDetailScreenState
+    extends State<AdminLeaveRequestDetailScreen> {
+  final TextEditingController _approvalOrRejectionMassage =
+      TextEditingController();
+  final _leaveStatusManager = getIt<LeaveStatusManager>();
+  final _stackManager = getIt<NavigationStackManager>();
+  final _leaveApplicationBloc = getIt<LeaveApplicationBloc>();
 
   @override
   Widget build(BuildContext context) {
@@ -36,28 +46,92 @@ class _AdminLeaveRequestDetailScreenState extends State<AdminLeaveRequestDetailS
       body: ListView(
         padding: const EdgeInsets.only(bottom: 100),
         children: [
-          LeaveTypeAgoTitle(timeAgo: widget.employeeLeave.leave.appliedOn.toDate.timeAgo(),leaveType: widget.employeeLeave.leave.leaveType),
-           UserContent(employee: widget.employeeLeave.employee),
-          RemainingLeaveContainer(employeeId: widget.employeeLeave.employee.id, leave: widget.employeeLeave.leave),
-          ReasonField(reason: widget.employeeLeave.leave.reason,),
-          (widget.employeeLeave.leave.leaveStatus != approveLeaveStatus)?_approvalRejectionMessage(context: context):Container(),
+          LeaveTypeAgoTitle(
+              timeAgo: widget.employeeLeave.leave.appliedOn.toDate.timeAgo(),
+              leaveType: widget.employeeLeave.leave.leaveType),
+          UserContent(employee: widget.employeeLeave.employee),
+          RemainingLeaveContainer(
+              employeeId: widget.employeeLeave.employee.id,
+              leave: widget.employeeLeave.leave),
+          ReasonField(
+            reason: widget.employeeLeave.leave.reason,
+          ),
+          (widget.employeeLeave.leave.leaveStatus != approveLeaveStatus)
+              ? _approvalRejectionMessage(context: context)
+              : Container(),
         ],
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-      floatingActionButton: (widget.employeeLeave.leave.leaveStatus != approveLeaveStatus)?ButtonContent(
-        leaveId: widget.employeeLeave.leave.leaveId,
-        reason: _approvalOrRejectionMassage.text,
-      ):null,
+      floatingActionButton:
+          (widget.employeeLeave.leave.leaveStatus != approveLeaveStatus)
+              ? _buildButton(context, _approvalOrRejectionMassage.text,
+                  widget.employeeLeave.leave.leaveId)
+              : null,
     );
   }
-  _approvalRejectionMessage({required BuildContext context}){
+
+  _buildButton(BuildContext context, String reason, String leaveId) {
+    final _localization = AppLocalizations.of(context);
+    return Container(
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.symmetric(horizontal: 50),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              fixedSize: Size(MediaQuery.of(context).size.width * 0.3, 45),
+              backgroundColor: AppColors.redColor,
+            ),
+            onPressed: () {
+              _leaveStatusManager.updateStatus(rejectLeaveStatus);
+              _leaveStatusManager.setReason(reason);
+              if (_leaveStatusManager.leaveApprove(leaveId)) {
+                _leaveApplicationBloc.deleteLeaveApplication(leaveId);
+                _stackManager.pop();
+              }
+            },
+            child: Text(
+              _localization.admin_leave_detail_button_reject,
+              style: AppTextStyle.subtitleText,
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              fixedSize: Size(MediaQuery.of(context).size.width * 0.3, 45),
+              backgroundColor: AppColors.greenColor,
+            ),
+            onPressed: () {
+              _leaveStatusManager.updateStatus(approveLeaveStatus);
+              _leaveStatusManager.setReason(reason);
+              if (_leaveStatusManager.leaveApprove(leaveId)) {
+                _leaveApplicationBloc.deleteLeaveApplication(leaveId);
+                _stackManager.pop();
+              }
+            },
+            child: Text(
+              _localization.admin_leave_detail_button_approve,
+              style: AppTextStyle.subtitleText,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _approvalRejectionMessage({required BuildContext context}) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: primaryHorizontalSpacing),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(AppLocalizations.of(context).admin_leave_detail_message_title_text, style: AppTextStyle.secondarySubtitle500,),
-          const SizedBox(height: 10,),
+          Text(
+            AppLocalizations.of(context).admin_leave_detail_message_title_text,
+            style: AppTextStyle.secondarySubtitle500,
+          ),
+          const SizedBox(
+            height: 10,
+          ),
           TextField(
             controller: _approvalOrRejectionMassage,
             maxLines: 5,
@@ -67,7 +141,8 @@ class _AdminLeaveRequestDetailScreenState extends State<AdminLeaveRequestDetailS
                 borderRadius: BorderRadius.circular(12),
                 borderSide: BorderSide.none,
               ),
-              hintText: AppLocalizations.of(context).admin_leave_detail_error_reason,
+              hintText:
+                  AppLocalizations.of(context).admin_leave_detail_error_reason,
               filled: true,
               fillColor: AppColors.primaryBlue.withOpacity(0.10),
             ),
