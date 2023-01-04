a04044ea8997da17d2e8c83a53c76069e25e43b5
Pratik-canopas <pratik.k@canopas.com>
2023-01-04 15:14:56 +0530

    removed unused imports

--- lib/ui/admin/edit_employe_details/edit_employee_details_view.dart
+++ lib/ui/admin/edit_employe_details/edit_employee_details_view.dart
@@ -3,15 +3,12 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:projectunity/configs/text_style.dart';
 import 'package:projectunity/core/utils/const/space_constant.dart';
 import 'package:projectunity/di/service_locator.dart';
-import 'package:projectunity/event_bus/events.dart';
 import 'package:projectunity/model/employee/employee.dart';
 import 'package:projectunity/ui/admin/edit_employe_details/bloc/edit_employee_details_bloc.dart';
 import 'package:projectunity/ui/admin/edit_employe_details/bloc/edit_employee_details_events.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:projectunity/ui/admin/edit_employe_details/widgets/admin_edit_employee_details_form.dart';
-import 'package:projectunity/ui/admin/employee/detail/bloc/employee_detail_event.dart';
 import '../../../configs/colors.dart';
-import '../../../widget/circular_progress_indicator.dart';
 import 'bloc/edit_employee_details_state.dart';
 
 class AdminEditEmployeeDetailsPage extends StatelessWidget {
