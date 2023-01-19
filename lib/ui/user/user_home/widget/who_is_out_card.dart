424379e6189dc1be5863dee00e13d526021982b9
Pratik-canopas <pratik.k@canopas.com>
2023-01-19 15:08:20 +0530

    make samaller widget

--- lib/ui/user/user_home/widget/who_is_out_card.dart
+++ lib/ui/user/user_home/widget/who_is_out_card.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
+import 'package:projectunity/widget/app_divider.dart';
 import '../../../../configs/colors.dart';
 import '../../../../configs/text_style.dart';
 import '../../../../configs/theme.dart';
 import '../../../../core/utils/const/space_constant.dart';
 import '../../../../core/utils/date_formatter.dart';
+import '../../../../model/leave_application.dart';
 import '../../../../widget/circular_progress_indicator.dart';
 import '../bloc/user_home_state.dart';
 import '../bloc/user_home_event.dart';
@@ -33,135 +35,154 @@ class WhoIsOutCard extends StatelessWidget {
               style: AppTextStyle.titleDark,
             ),
           ),
-          const Divider(
-            color: AppColors.dividerColor,
-            indent: primaryHorizontalSpacing,
-            endIndent: primaryHorizontalSpacing,
-            thickness: 1,
-            height: 1,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-                horizontal: primaryHorizontalSpacing,
-                vertical: primaryVerticalSpacing),
-            child: Row(
-              children: [
-                IconButton(
-                    onPressed: () {
-                      context.read<UserHomeBloc>().add(ChangeToBeforeDateEvent());
-                    },
-                    icon: const Icon(Icons.arrow_back_ios_new_rounded, size: 18)),
-                BlocBuilder<UserHomeBloc, UserHomeState>(
-                    buildWhen: (previous, current) =>
-                        previous.dateOfAbsenceEmployee !=
-                        current.dateOfAbsenceEmployee,
-                    builder: (context, state) => SizedBox(
-                          width: 110,
-                          child: Center(
-                            child: Text(
-                              DateFormatter(AppLocalizations.of(context)).getDateRepresentation(state.dateOfAbsenceEmployee),
-                              style: AppTextStyle.mediumDark,
-                            ),
-                          ),
-                        )),
-                IconButton(
-                    onPressed: () {
-                      context.read<UserHomeBloc>().add(ChangeToAfterDateEvent());
-                    },
-                    icon: const Icon(
-                      Icons.arrow_forward_ios_rounded,
-                      size: 18,
-                    )),
-                const Spacer(),
-                MaterialButton(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    height: 30,
-                    highlightElevation: 0,
-                    elevation: 0,
-                    color: AppColors.lightPrimaryBlue,
-                    onPressed: () {
-                      ///TODO open absence employee list of selected employee
-                    },
-                    child: Text(
-                      AppLocalizations.of(context).see_all_button_tag,
-                      style: AppTextStyle.bodyDark,
-                    )),
-              ],
-            ),
-          ),
-          const Divider(
-            color: AppColors.dividerColor,
-            indent: primaryHorizontalSpacing,
-            endIndent: primaryHorizontalSpacing,
-            thickness: 1,
-            height: 1,
-          ),
+          const AppDivider(),
+          const _WhoIsOutCardControlButtons(),
+          const AppDivider(),
           const SizedBox(
             height: primaryVerticalSpacing,
           ),
           BlocBuilder<UserHomeBloc, UserHomeState>(
-            builder: (context, state) => state.status ==
-                    UserHomeStatus.loading
+            builder: (context, state) => state.status == UserHomeStatus.loading
                 ? const Padding(
                     padding: EdgeInsets.all(primaryHorizontalSpacing),
                     child: AppCircularProgressIndicator(),
                   )
-                : Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                            crossAxisAlignment: state.absence.isEmpty?CrossAxisAlignment.center:CrossAxisAlignment.start,
-                            children: [
-                              SingleChildScrollView(
-                                      padding:  EdgeInsets.all(state.absence.isEmpty?0:primaryVerticalSpacing).copyWith(bottom: 0),
-                                      scrollDirection: Axis.horizontal,
-                                      child: Row(
-                                        children: state.absence
-                                            .map((e) => Container(
-                                                  margin: const EdgeInsets.symmetric(horizontal: primaryVerticalSpacing),
-                                                  height: 60,
-                                                  width: 60,
-                                                  decoration: BoxDecoration(
-                                                    image: (e.employee.imageUrl != null)
-                                                            ? DecorationImage(image: NetworkImage(e.employee.imageUrl!),fit: BoxFit.cover)
-                                                            : null,
-                                                    color: AppColors.primaryGray,
-                                                    borderRadius: AppTheme.commonBorderRadius,
-                                                  ),
-                                                  child: (e.employee.imageUrl != null)
-                                                      ? null
-                                                      : const Icon(Icons.person, size: 30, color: Colors.black54),
-                                                )).toList(),
-                                      ),
-                                    ),
-                              Padding(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: primaryHorizontalSpacing,
-                                    vertical: primaryVerticalSpacing),
-                                child: Text(
-                                  AppLocalizations.of(context)
-                                      .who_is_out_card_message(
-                                          DateFormatter(
-                                                  AppLocalizations.of(context))
-                                              .getDateRepresentation(
-                                                  state.dateOfAbsenceEmployee),
-                                          state.absence.length,
-                                          state.absence.length - 1,
-                                          (state.absence.isEmpty)
-                                              ? ""
-                                              : state.absence.first.employee.name),
-                                  style: AppTextStyle.mediumDark,
-                                ),
-                              ),
-                            ]),
-                      ),
-                    ],
+                : _AbsenceEmployeesListWhoIsOutCardView(
+                    absence: state.absence,
+                    dateOfEmployeeAbsence: state.dateOfAbsenceEmployee,
                   ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class _WhoIsOutCardControlButtons extends StatelessWidget {
+  const _WhoIsOutCardControlButtons({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+          horizontal: primaryHorizontalSpacing,
+          vertical: primaryVerticalSpacing),
+      child: Row(
+        children: [
+          IconButton(
+              onPressed: () {
+                context.read<UserHomeBloc>().add(ChangeToBeforeDateEvent());
+              },
+              icon: const Icon(Icons.arrow_back_ios_new_rounded, size: 18)),
+          BlocBuilder<UserHomeBloc, UserHomeState>(
+              buildWhen: (previous, current) =>
+                  previous.dateOfAbsenceEmployee !=
+                  current.dateOfAbsenceEmployee,
+              builder: (context, state) => SizedBox(
+                    width: 110,
+                    child: Center(
+                      child: Text(
+                        DateFormatter(AppLocalizations.of(context))
+                            .getDateRepresentation(state.dateOfAbsenceEmployee),
+                        style: AppTextStyle.mediumDark,
+                      ),
+                    ),
+                  )),
+          IconButton(
+              onPressed: () {
+                context.read<UserHomeBloc>().add(ChangeToAfterDateEvent());
+              },
+              icon: const Icon(
+                Icons.arrow_forward_ios_rounded,
+                size: 18,
+              )),
+          const Spacer(),
+          MaterialButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5),
+              ),
+              height: 30,
+              highlightElevation: 0,
+              elevation: 0,
+              color: AppColors.lightPrimaryBlue,
+              onPressed: () {
+                ///TODO open absence employee list of selected employee
+              },
+              child: Text(
+                AppLocalizations.of(context).see_all_button_tag,
+                style: AppTextStyle.bodyDark,
+              )),
+        ],
+      ),
+    );
+  }
+}
+
+class _AbsenceEmployeesListWhoIsOutCardView extends StatelessWidget {
+  final DateTime dateOfEmployeeAbsence;
+  final List<LeaveApplication> absence;
+
+  const _AbsenceEmployeesListWhoIsOutCardView(
+      {Key? key, required this.dateOfEmployeeAbsence, required this.absence})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+              crossAxisAlignment: absence.isEmpty
+                  ? CrossAxisAlignment.center
+                  : CrossAxisAlignment.start,
+              children: [
+                SingleChildScrollView(
+                  padding: EdgeInsets.all(
+                          absence.isEmpty ? 0 : primaryVerticalSpacing)
+                      .copyWith(bottom: 0),
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: absence
+                        .map((e) => Container(
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: primaryVerticalSpacing),
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                image: (e.employee.imageUrl != null)
+                                    ? DecorationImage(
+                                        image:
+                                            NetworkImage(e.employee.imageUrl!),
+                                        fit: BoxFit.cover)
+                                    : null,
+                                color: AppColors.primaryGray,
+                                borderRadius: AppTheme.commonBorderRadius,
+                              ),
+                              child: (e.employee.imageUrl != null)
+                                  ? null
+                                  : const Icon(Icons.person,
+                                      size: 30, color: Colors.black54),
+                            ))
+                        .toList(),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: primaryHorizontalSpacing,
+                      vertical: primaryVerticalSpacing),
+                  child: Text(
+                    AppLocalizations.of(context).who_is_out_card_message(
+                        DateFormatter(AppLocalizations.of(context))
+                            .getDateRepresentation(dateOfEmployeeAbsence),
+                        absence.length,
+                        absence.length - 1,
+                        (absence.isEmpty) ? "" : absence.first.employee.name),
+                    style: AppTextStyle.mediumDark,
+                  ),
+                ),
+              ]),
+        ),
+      ],
     );
   }
 }
