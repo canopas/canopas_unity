657c44180c77f4bca517be4111eb181531913077
Pratik-canopas <pratik.k@canopas.com>
2023-01-19 13:54:54 +0530

    added error handling

--- lib/ui/user/user_employees/user_employees_screen.dart
+++ lib/ui/user/user_employees/user_employees_screen.dart
@@ -7,10 +7,12 @@ import 'package:projectunity/ui/user/user_employees/bloc/user_employee_state.dar
 import 'package:projectunity/ui/user/user_employees/bloc/user_employees_bloc.dart';
 import 'package:projectunity/ui/user/user_employees/bloc/user_employees_event.dart';
 import 'package:projectunity/widget/circular_progress_indicator.dart';
+import 'package:projectunity/widget/error_snack_bar.dart';
 import 'package:projectunity/widget/user_profile_image.dart';
 import '../../../configs/colors.dart';
 import '../../../core/utils/const/space_constant.dart';
 import '../../../di/service_locator.dart';
+import '../../../model/employee/employee.dart';
 
 class UserEmployeesPage extends StatelessWidget {
   const UserEmployeesPage({Key? key}) : super(key: key);
@@ -53,63 +55,84 @@ class _UserEmployeesScreenState extends State<UserEmployeesScreen> {
         onRefresh: () async {
           context.read<UserEmployeesBloc>().add(FetchEmployeesEvent());
         },
-        child: BlocBuilder<UserEmployeesBloc, UserEmployeesState>(
+        child: BlocConsumer<UserEmployeesBloc, UserEmployeesState>(
+            listenWhen: (previous, current) =>
+                current is UserEmployeesFailureState,
+            listener: (context, state) {
+              if (state is UserEmployeesFailureState) {
+                showSnackBar(context: context, error: state.error);
+              }
+            },
             builder: (context, state) {
-          if (state is UserEmployeesLoadingState) {
-            return const AppCircularProgressIndicator();
-          } else if (state is UserEmployeesSuccessState) {
-            return ListView.separated(
-                padding: const EdgeInsets.symmetric(
-                    horizontal: primaryVerticalSpacing),
-                itemBuilder: (BuildContext context, int employee) => InkWell(
-                      borderRadius: AppTheme.commonBorderRadius,
-                      onTap: () {
-                        ///TODO implement navigation to employee details screen
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: primaryVerticalSpacing,
-                            vertical: primaryHalfSpacing),
-                        child: Row(
-                          children: [
-                            ImageProfile(
-                                imageUrl: state.employees[employee].imageUrl,
-                                radius: 25,
-                                borderColor: AppColors.textDark,
-                                borderSize: 1,
-                                backgroundColor: AppColors.dividerColor,
-                                iconColor: AppColors.greyColor),
-                            const SizedBox(
-                              width: primaryHorizontalSpacing,
-                            ),
-                            Flexible(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(state.employees[employee].name,
-                                      style: AppTextStyle.titleDark),
-                                  const SizedBox(height: 2),
-                                  Text(state.employees[employee].designation,
-                                      style: AppTextStyle.bodyDark),
-                                ],
-                              ),
-                            )
-                          ],
+              if (state is UserEmployeesLoadingState) {
+                return const AppCircularProgressIndicator();
+              } else if (state is UserEmployeesSuccessState) {
+                return ListView.separated(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: primaryVerticalSpacing),
+                    itemBuilder: (BuildContext context, int employee) =>
+                        EmployeeCard(
+                          employee: state.employees[employee],
+                          onTap: () {
+                            ///TODO implement navigation to employee details screen
+                          },
                         ),
-                      ),
-                    ),
-                separatorBuilder: (context, index) => const Divider(
-                    color: AppColors.dividerColor,
-                    indent: primaryHorizontalSpacing,
-                    endIndent: primaryHorizontalSpacing,
-                    thickness: 1,
-                    height: 1),
-                itemCount: state.employees.length);
-          }
+                    separatorBuilder: (context, index) => const Divider(
+                        color: AppColors.dividerColor,
+                        indent: primaryHorizontalSpacing,
+                        endIndent: primaryHorizontalSpacing,
+                        thickness: 1,
+                        height: 1),
+                    itemCount: state.employees.length);
+              }
 
-          ///TODO implement empty employees screen.
-          return const SizedBox();
-        }),
+              ///TODO implement empty employees screen.
+              return const SizedBox();
+            }),
+      ),
+    );
+  }
+}
+
+class EmployeeCard extends StatelessWidget {
+  final void Function()? onTap;
+  final Employee employee;
+
+  const EmployeeCard({Key? key, this.onTap, required this.employee})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: AppTheme.commonBorderRadius,
+      onTap: onTap,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+            horizontal: primaryVerticalSpacing, vertical: primaryHalfSpacing),
+        child: Row(
+          children: [
+            ImageProfile(
+                imageUrl: employee.imageUrl,
+                radius: 25,
+                borderColor: AppColors.textDark,
+                borderSize: 1,
+                backgroundColor: AppColors.dividerColor,
+                iconColor: AppColors.greyColor),
+            const SizedBox(
+              width: primaryHorizontalSpacing,
+            ),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(employee.name, style: AppTextStyle.titleDark),
+                  const SizedBox(height: 2),
+                  Text(employee.designation, style: AppTextStyle.bodyDark),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
