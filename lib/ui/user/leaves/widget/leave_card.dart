c4f1f4a80f184c1d008a0ce2c3d2be0c1218ce35
sneha.s <sneha.s@canopas.com>
2023-02-01 12:00:44 +0530

    WIP- user leaves screen- Refactoring

--- lib/ui/user/leaves/widget/leave_card.dart
+++ lib/ui/user/leaves/widget/leave_card.dart
@@ -1,10 +1,10 @@
-import 'package:flutter_gen/gen_l10n/app_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localization.dart';
 
 import '../../../../configs/colors.dart';
 import '../../../../configs/text_style.dart';
 import '../../../../core/utils/date_formatter.dart';
-
+import '../../../../model/leave/leave.dart';
 
 class LeaveCard extends StatelessWidget {
   final double totalDays;
@@ -24,19 +24,20 @@ class LeaveCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var localization= AppLocalizations.of(context);
-    VoidCallback? ontap;
-    final String leaveDuration= DateFormatter(localization).getLeaveDurationPresentation(totalDays);
-    final String leaveStatus= localization.leave_type_placeholder_leave_status(type);
-    final String leavePeriod= DateFormatter(localization).dateInLine(startTimeStamp: startDate, endTimeStamp: endDate);
+    var localization = AppLocalizations.of(context);
+    final String leaveDuration =
+        DateFormatter(localization).getLeaveDurationPresentation(totalDays);
+    final String leaveType =
+        localization.leave_type_placeholder_leave_status(type);
+    final String leavePeriod = DateFormatter(localization)
+        .dateInLine(startTimeStamp: startDate, endTimeStamp: endDate);
     return Card(
       child: Padding(
         padding: const EdgeInsets.all(10.0),
         child: SizedBox(
           height: 80,
           child: Row(
-            mainAxisAlignment:
-            MainAxisAlignment.spaceBetween,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Column(
                 crossAxisAlignment:
@@ -49,45 +50,67 @@ class LeaveCard extends StatelessWidget {
                           color: AppColors.greyColor,
                           fontWeight: FontWeight.w500)),
                   Text(leavePeriod,
-                      style: TextStyle(
+                      style: const TextStyle(
                           color: AppColors.blackColor,
                           fontSize: 15,
                           fontWeight: FontWeight.bold)),
                   Text(
-                    'Casual',
-                    style: TextStyle(
-                        color:
-                        AppColors.primaryDarkYellow),
+                    leaveType,
+                    style: const TextStyle(color: AppColors.primaryDarkYellow),
                   )
                 ],
               ),
               Column(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
-                  Row(
-                    children: [
-                      const Icon(
-                        Icons.error,
-                        color: AppColors.secondaryText,
-                      ),
-                      const SizedBox(
-                        width: 5,
-                      ),
-                      Text(
-                        leaveStatus,
-                        style: AppTextStyle.bodyTextDark,
-                      )
-                    ],
-                  ),
+                  LeaveStatusIcon(leaveStatus: status),
                   IconButton(
-                      onPressed: ontap, icon: const Icon(Icons.arrow_forward_ios_outlined))
-
+                      onPressed: () {},
+                      icon: const Icon(Icons.arrow_forward_ios_outlined))
                 ],
               )
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class LeaveStatusIcon extends StatelessWidget {
+  final int leaveStatus;
+
+  const LeaveStatusIcon({Key? key, required this.leaveStatus})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        if (leaveStatus == pendingLeaveStatus)
+          const Icon(
+            Icons.error,
+            color: AppColors.secondaryText,
+          )
+        else if (leaveStatus == rejectLeaveStatus)
+          const Icon(
+            Icons.dangerous,
+            color: AppColors.primaryPink,
+          )
+        else if (leaveStatus == approveLeaveStatus)
+          const Icon(
+            Icons.check_circle,
+            color: AppColors.primaryGreen,
+          ),
+        const SizedBox(
+          width: 5,
+        ),
+        Text(
+          AppLocalizations.of(context)
+              .leave_status_placeholder_text(leaveStatus),
+          style: AppTextStyle.bodyTextDark,
+        )
+      ],
     );
   }
 }
