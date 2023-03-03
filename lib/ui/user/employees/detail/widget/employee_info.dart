d26ee2dded64641aa371a98b5cd8bb3f6e7b8105
Pratik-canopas <pratik.k@canopas.com>
2023-03-03 17:04:44 +0530

    fix pipeline

--- lib/ui/user/employees/detail/widget/employee_info.dart
+++ lib/ui/user/employees/detail/widget/employee_info.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
-
 import '../../../../../model/employee/employee.dart';
 import '../../../../../widget/employee_details_field.dart';
-import '../../../../admin/employee/detail/widget/profile_detail.dart';
 
 class EmployeeInfo extends StatelessWidget {
   final Employee employee;
