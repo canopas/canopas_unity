0118d8cb4069b1ace82c1027b5cd00db638b4bff
Pratik-canopas <pratik.k@canopas.com>
2023-03-16 12:02:59 +0530

    Add loading animation in who is out card

--- lib/widget/circular_progress_indicator.dart
+++ lib/widget/circular_progress_indicator.dart
@@ -1,18 +1,91 @@
 import 'package:flutter/material.dart';
 import '../configs/colors.dart';
+import 'dart:math' as math show sin, pi;
 
-class AppCircularProgressIndicator extends StatelessWidget {
+class DelayTween extends Tween<double> {
+  DelayTween({double? begin, double? end, required this.delay})
+      : super(begin: begin, end: end);
+  final double delay;
+
+  @override
+  double lerp(double t) =>
+      super.lerp((math.sin((t - delay) * 2 * math.pi) + 1) / 2);
+
+  @override
+  double evaluate(Animation<double> animation) => lerp(animation.value);
+}
+
+class ThreeBounceLoading extends StatefulWidget {
+  final Color? color;
   final double size;
-  const AppCircularProgressIndicator({Key? key, this.size = 38}) : super(key: key);
+  final Duration duration;
+
+  const ThreeBounceLoading({
+    Key? key,
+    this.color,
+    this.size = 50.0,
+    this.duration = const Duration(milliseconds: 1400),
+  }) : super(key: key);
+
+  @override
+  State<ThreeBounceLoading> createState() => _ThreeBounceLoadingState();
+}
+
+class _ThreeBounceLoadingState extends State<ThreeBounceLoading>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this, duration: widget.duration)
+      ..repeat();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
+    return Center(
+      child: SizedBox.fromSize(
+        size: Size(widget.size * 2, widget.size),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: List.generate(3, (i) {
+            return ScaleTransition(
+              scale: DelayTween(begin: 0.0, end: 1.0, delay: i * .2)
+                  .animate(_controller),
+              child: SizedBox.fromSize(
+                  size: Size.square(widget.size * 0.5),
+                  child: DecoratedBox(
+                      decoration: BoxDecoration(
+                          color: widget.color, shape: BoxShape.circle))),
+            );
+          }),
+        ),
+      ),
+    );
+  }
+}
+
+class AppCircularProgressIndicator extends StatelessWidget {
+  final double size;
+
+  const AppCircularProgressIndicator({Key? key, this.size = 38})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
       child: SizedBox(
         height: size,
         width: size,
-        child:  CircularProgressIndicator(
-          strokeWidth: size/9,
+        child: CircularProgressIndicator(
+          strokeWidth: size / 9,
           color: AppColors.primaryBlue,
         ),
       ),
