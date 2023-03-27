260969303b35e308ee3c7294224a24b13477e950
cp-yogita-k <yogita.k@canopas.com>
2023-03-27 14:53:54 +0530

    remove space

--- lib/ui/user/leaves/detail/user_leave_detail_screen.dart
+++ lib/ui/user/leaves/detail/user_leave_detail_screen.dart
@@ -75,6 +75,7 @@ class _UserLeaveDetailScreenState extends State<UserLeaveDetailScreen> {
                 return const AppCircularProgressIndicator();
               } else if (state is UserLeaveDetailSuccessState) {
                 Leave leave = state.leave;
+                final note=leave.rejectionReason??"";
                 bool showCancelButton = state.showCancelButton;
                 return ListView(
                   children: [
@@ -90,7 +91,7 @@ class _UserLeaveDetailScreenState extends State<UserLeaveDetailScreen> {
                       title: localization.reason_tag,
                       reason: leave.reason,
                     ),
-                    if (leave.rejectionReason!.trim().isNotEmpty || leave.rejectionReason!=null)
+                    if (note.trim().isNotEmpty)
                       ResponseNote(leaveResponse: leave.rejectionReason!),
                     if (showCancelButton) CancelButton(leaveId: leave.leaveId)
                   ],
