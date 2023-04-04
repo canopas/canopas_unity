0896066608f03af911bcaf3a9a9543a9dd5297ac
sneha.s <sneha.s@canopas.com>
2023-04-04 15:30:44 +0530

    Minor change

--- lib/ui/sign_in/bloc/sign_in_view_bloc.dart
+++ lib/ui/sign_in/bloc/sign_in_view_bloc.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:injectable/injectable.dart';
 
 import '../../../data/core/exception/custom_exception.dart';
-import '../../../data/core/exception/error_const.dart';
 import '../../../data/provider/user_data.dart';
 import '../../../data/services/auth_service.dart';
 import '../../../data/stateManager/auth/auth_manager.dart';
@@ -38,32 +37,32 @@ class SignInBloc extends Bloc<SignInEvent, SignInState> {
     }
   }
 
-  Future<void> _signIn(SignInEvent event, Emitter<SignInState> emit) async {
-    User? user;
-    emit(SignInLoadingState());
-    try {
-      user = await _authService.signInWithGoogle();
-      _authManager.setUserAuthenticated(user);
-      emit(SignInSuccessState());
-      _userManager.hasLoggedIn();
-    } on CustomException catch (error) {
-      emit(SignInFailureState(error: error.errorMessage.toString()));
-    }
-    if (user == null) {
-      emit(SignInInitialState());
-    } else {
-      final data = await _authManager.getUser(user.email!);
-      try {
-        if (data != null) {
-          await _authManager.updateUser(data);
-          emit(SignInSuccessState());
-          _userManager.hasLoggedIn();
-        } else {
-          emit(SignInFailureState(error: userNotFoundError));
-        }
-      } on Exception {
-        emit(SignInFailureState(error: userDataNotUpdateError));
-      }
-    }
-  }
+  // Future<void> _signIn(SignInEvent event, Emitter<SignInState> emit) async {
+  //   User? user;
+  //   emit(SignInLoadingState());
+  //   try {
+  //     user = await _authService.signInWithGoogle();
+  //     _authManager.setUserAuthenticated(user);
+  //     emit(SignInSuccessState());
+  //     _userManager.hasLoggedIn();
+  //   } on CustomException catch (error) {
+  //     emit(SignInFailureState(error: error.errorMessage.toString()));
+  //   }
+  //   if (user == null) {
+  //     emit(SignInInitialState());
+  //   } else {
+  //     final data = await _authManager.getUser(user.email!);
+  //     try {
+  //       if (data != null) {
+  //         await _authManager.updateUser(data);
+  //         emit(SignInSuccessState());
+  //         _userManager.hasLoggedIn();
+  //       } else {
+  //         emit(SignInFailureState(error: userNotFoundError));
+  //       }
+  //     } on Exception {
+  //       emit(SignInFailureState(error: userDataNotUpdateError));
+  //     }
+  //   }
+  // }
 }
