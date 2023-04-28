cb2fc6c7e26ee0fc9e480bb156d55d0e54241a51
Pratik-canopas <pratik.k@canopas.com>
2023-04-28 16:35:03 +0530

    Resolve conflict

--- lib/ui/admin/leaves/leave_screen/admin_leaves_screen.dart
+++ lib/ui/admin/leaves/leave_screen/admin_leaves_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
-import 'package:go_router/go_router.dart';
 import 'package:projectunity/data/core/utils/bloc_status.dart';
 import 'package:projectunity/data/di/service_locator.dart';
 import 'package:projectunity/ui/admin/leaves/leave_screen/widget/expandable_list.dart';
