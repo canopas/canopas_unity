cf45f1b9805f3851a3d0e43c119eb58a2bc827e5
Pratik-canopas <pratik.k@canopas.com>
2023-05-05 10:58:36 +0530

    Remove validate widget

--- lib/ui/user/leaves/apply_leave/widget/bottom_button.dart
+++ lib/ui/user/leaves/apply_leave/widget/bottom_button.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localization.dart';
-import 'package:projectunity/ui/widget/widget_validation.dart';
 import '../../../../../data/configs/colors.dart';
 import '../../../../../data/configs/space_constant.dart';
 import '../../../../../data/configs/text_style.dart';
@@ -17,27 +16,26 @@ class ApplyButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: primaryHorizontalSpacing),
-      child: BlocBuilder<ApplyLeaveBloc, ApplyLeaveState>(
-          builder: (context, state) => ValidateWidget(
-                isValid: state.leaveRequestStatus != Status.loading,
-                unValidWidget:
-                    const FittedBox(child: AppCircularProgressIndicator()),
-                child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        fixedSize: Size(MediaQuery.of(context).size.width, 50),
-                        elevation: 2),
-                    onPressed: () {
-                      context
-                          .read<ApplyLeaveBloc>()
-                          .add(ApplyLeaveSubmitFormEvent());
-                    },
-                    child: Text(
-                        AppLocalizations.of(context)
-                            .user_leaves_apply_leave_button_tag,
-                        style: AppFontStyle.buttonTextStyle
-                            .copyWith(color: AppColors.whiteColor))),
-              )),
-    );
+        padding:
+            const EdgeInsets.symmetric(horizontal: primaryHorizontalSpacing),
+        child: BlocBuilder<ApplyLeaveBloc, ApplyLeaveState>(
+          builder: (context, state) => state.leaveRequestStatus !=
+                  Status.loading
+              ? const FittedBox(child: AppCircularProgressIndicator())
+              : ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      fixedSize: Size(MediaQuery.of(context).size.width, 50),
+                      elevation: 2),
+                  onPressed: () {
+                    context
+                        .read<ApplyLeaveBloc>()
+                        .add(ApplyLeaveSubmitFormEvent());
+                  },
+                  child: Text(
+                      AppLocalizations.of(context)
+                          .user_leaves_apply_leave_button_tag,
+                      style: AppFontStyle.buttonTextStyle
+                          .copyWith(color: AppColors.whiteColor))),
+        ));
   }
 }
