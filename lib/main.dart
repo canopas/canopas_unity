c3bb2beeca4463f12515eaa8550c70c7c416166c
Pratik-canopas <pratik.k@canopas.com>
2023-05-05 14:01:03 +0530

    Fix pipeline

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:ui';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
