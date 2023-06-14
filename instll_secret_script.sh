525e7d0814e43f0b0bcc09ff7197c63ee0b00ea1
Sneha Canopas <sneha.s@canopas.com>
2023-06-14 15:42:27 +0530

    WIP-- Add script for craete version

---
+++ instll_secret_script.sh
@@ -0,0 +1,6 @@
+          echo $DESKTOP_CREDENTIALS_BASE64 | base64 -di > lib/desktop_credentials.dart
+          echo $FIREBASE_OPTIONS_BASE64 | base64 -di > lib/firebase_options.dart
+          echo $GOOGLE_SERVICES_JSON_BASE64 | base64 -di > android/app/google-services.json
+          echo $GOOGLE_SERVICES_PLIST_IOS_BASE64 | base64 -di > ios/Runner/GoogleService-Info.plist
+          echo $GOOGLE_SERVICES_PLIST_MACOS_BASE64 | base64 -di > macos/Runner/GoogleService-Info.plist
+          echo $WEB_INDEX_HTML_BASE64 | base64 -di > web/index.html
