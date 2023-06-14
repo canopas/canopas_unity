6e6eceedd459ed229670aeaa8b17b83181ed48ce
Radhika Canopas <radhika.s@canopas.com>
2023-06-14 17:56:30 +0530

    Cleanup

--- web/encrypt_var.sh
+++
@@ -1,3 +0,0 @@
-echo $DESKTOP_CREDENTIALS_BASE64  | base64 -di > lib/desktop_credentials.dart
-   echo $FIREBASE_OPTIONS_BASE64  | base64 -di > lib/firebase_options.dart
-echo $WEB_INDEX_HTML_BASE64  | base64 -di > web/index.html
