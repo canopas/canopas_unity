5b13a35a76cb6904a9bea39f3714e19dff13686f
Sneha Canopas <sneha.s@canopas.com>
2023-06-15 12:28:25 +0530

    WIP- Add ios build

--- ios_dist_certtificate_script.sh
+++ ios_dist_certtificate_script.sh
@@ -3,16 +3,16 @@
 CERTIFICATE_P12=dist_certificate.p12
 
 # Recreate the certificate from the secure environment variable
-echo $APPLE_DISTRIBUTION_CERT_N_KEY | base64 --decode > $CERTIFICATE_P12
+echo ${{ secrets.APPLE_DISTRIBUTION_CERT_N_KEY }} | base64 --decode > $CERTIFICATE_P12
 
 # Unlock the keychain
-security unlock-keychain -p $BUILD_KEY_CHAIN_PASS $BUILD_KEY_CHAIN
+security unlock-keychain -p ${{ secrets.BUILD_KEY_CHAIN_PASS }} ${{ secrets.BUILD_KEY_CHAIN }}
 
-security set-keychain-settings $BUILD_KEY_CHAIN
+security set-keychain-settings ${{ secrets.BUILD_KEY_CHAIN }}
 
-security import $CERTIFICATE_P12 -k $BUILD_KEY_CHAIN -P $APPLE_DISTRIBUTION_CERTIFICATE_PASSWORD -T /usr/bin/codesign;
+security import $CERTIFICATE_P12 -k ${{ secrets.BUILD_KEY_CHAIN }} -P ${{ secrets.APPLE_DISTRIBUTION_CERTIFICATE_PASSWORD }} -T /usr/bin/codesign;
 
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $BUILD_KEY_CHAIN_PASS $BUILD_KEY_CHAIN
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k ${{ secrets.BUILD_KEY_CHAIN_PASS }} ${{ secrets.BUILD_KEY_CHAIN }}
 
 # remove certs
 rm -fr *.p12
