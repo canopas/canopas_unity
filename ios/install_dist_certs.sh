6c5c003b87365189398953b61b594caf7493c94e
Sneha Canopas <sneha.s@canopas.com>
2023-06-15 13:04:37 +0530

    WIP- Add ios build

--- ios/install_dist_certs.sh
+++ ios/install_dist_certs.sh
@@ -1,3 +1,10 @@
+#!/usr/bin/env sh
+
+CERTIFICATE_P12=dist_certificate.p12
+
+# Recreate the certificate from the secure environment variable
+echo $APPLE_DISTRIBUTION_CERT_N_KEY | base64 --decode > $CERTIFICATE_P12
+
 # Unlock the keychain
 security unlock-keychain -p $BUILD_KEY_CHAIN_PASS $BUILD_KEY_CHAIN
 
