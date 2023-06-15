87e50f0706d703585b2bc7423f24df16c2b41a8d
Sneha Canopas <sneha.s@canopas.com>
2023-06-15 12:36:49 +0530

    WIP- Add ios build

--- ios_dist_profile_script.sh
+++ ios_dist_profile_script.sh
@@ -1,13 +1,13 @@
 
 #!/usr/bin/env sh
 
-DIST_PROFILE_FILE=${{ secrets.DIST_PROVISION_UUID }}.mobileprovision
+DIST_PROFILE_FILE=${DIST_PROVISION_UUID}.mobileprovision
 
 # Recreate the certificate from the secure environment variable
-echo ${{ secrets.DIST_PROVISION }} | base64 --decode > $DIST_PROFILE_FILE
+echo $DIST_PROVISION | base64 --decode > $DIST_PROFILE_FILE
 
 # copy where Xcode can find it
-cp ${DIST_PROFILE_FILE} "$HOME/Library/MobileDevice/Provisioning Profiles/${{ secrets.DIST_PROVISION_UUID }}.mobileprovision"
+cp ${DIST_PROFILE_FILE} "$HOME/Library/MobileDevice/Provisioning Profiles/${DIST_PROVISION_UUID}.mobileprovision"
 
 # clean
 rm -fr *.mobileprovision
