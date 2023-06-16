4901e7cc32c0a07f1aed8f2136272dbf6604142a
Radhika Canopas <radhika.s@canopas.com>
2023-06-16 14:58:02 +0530

    Minor fix

--- ios/install_dist_profile.sh
+++ ios/install_dist_profile.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-DIST_PROFILE_FILE=${DIST_PROVISION_UUID}.mobileprovision
+DIST_PROFILE_FILE=$DIST_PROVISION_UUID.mobileprovision
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 echo $DIST_PROVISION | base64 --decode > ~/Library/MobileDevice/Provisioning\ Profiles/$DIST_PROFILE_FILE
