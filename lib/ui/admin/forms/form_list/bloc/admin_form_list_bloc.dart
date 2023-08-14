76a607b19ede4a117528afa2357f0878f17706b8
Pratik-canopas <pratik.k@canopas.com>
2023-08-14 12:32:11 +0530

    Create form list screen bloc

---
+++ lib/ui/admin/forms/form_list/bloc/admin_form_list_bloc.dart
@@ -0,0 +1,48 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:projectunity/data/core/exception/error_const.dart';
+import 'package:projectunity/data/core/extensions/list.dart';
+import 'package:projectunity/data/model/org_forms/org_form_field/org_form_field.dart';
+import 'package:projectunity/data/model/org_forms/org_form_info/org_form_info.dart';
+import 'package:projectunity/ui/admin/forms/form_list/bloc/admin_form_list_event.dart';
+import 'package:projectunity/ui/admin/forms/form_list/bloc/admin_form_list_state.dart';
+import '../../../../../data/core/utils/bloc_status.dart';
+import '../../../../../data/repo/form_repo.dart';
+
+class AdminFormListBloc extends Bloc<AdminFormListEvents, AdminFormListState> {
+  final FormRepo _formRepo;
+
+  AdminFormListBloc(this._formRepo) : super(const AdminFormListState()) {
+    on<AdminFormListInitialLoadEvent>(_init);
+    on<UpdateFormEvent>(_updateForm);
+  }
+
+  Future<void> _init(AdminFormListInitialLoadEvent event,
+      Emitter<AdminFormListState> emit) async {
+    emit(state.copyWith(status: Status.loading));
+    try {
+      final forms = await _formRepo.getForms();
+      emit(state.copyWith(status: Status.success, forms: forms));
+    } on Exception {
+      emit(
+          state.copyWith(status: Status.error, error: firestoreFetchDataError));
+    }
+  }
+
+  Future<void> _updateForm(UpdateFormEvent event,
+      Emitter<AdminFormListState> emit) async {
+
+    try {
+      List<OrgFormInfo> forms = state.forms.toList();
+      final form = await _formRepo.getForm(formId: event.formId);
+      forms.removeWhereAndAdd(form, (existForm) => existForm.id == form?);
+
+      emit(state.copyWith(status: Status.success, forms: forms));
+    } on Exception {
+      emit(
+          state.copyWith(status: Status.error, error: firestoreFetchDataError));
+    }
+  }
+
+
+}
+
