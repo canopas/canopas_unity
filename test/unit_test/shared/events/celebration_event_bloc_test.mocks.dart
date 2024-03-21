eeff1ae96d0f7960676050fe5f27ff8aab412438
cp-sneha-s <sneha.s@canopas.com>
2024-03-21 12:13:18 +0530

    Fix Upcoming Events

--- test/unit_test/shared/events/celebration_event_bloc_test.mocks.dart
+++ test/unit_test/shared/events/celebration_event_bloc_test.mocks.dart
@@ -3,11 +3,12 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i3;
+import 'dart:async' as _i4;
 
+import 'package:cloud_firestore/cloud_firestore.dart' as _i2;
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:projectunity/data/model/employee/employee.dart' as _i4;
-import 'package:projectunity/data/repo/employee_repo.dart' as _i2;
+import 'package:projectunity/data/model/employee/employee.dart' as _i5;
+import 'package:projectunity/data/services/employee_service.dart' as _i3;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -22,58 +23,133 @@ import 'package:projectunity/data/repo/employee_repo.dart' as _i2;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
-/// A class which mocks [EmployeeRepo].
+class _FakeFirebaseFirestore_0 extends _i1.SmartFake
+    implements _i2.FirebaseFirestore {
+  _FakeFirebaseFirestore_0(
+    Object parent,
+    Invocation parentInvocation,
+  ) : super(
+          parent,
+          parentInvocation,
+        );
+}
+
+/// A class which mocks [EmployeeService].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockEmployeeRepo extends _i1.Mock implements _i2.EmployeeRepo {
-  MockEmployeeRepo() {
+class MockEmployeeService extends _i1.Mock implements _i3.EmployeeService {
+  MockEmployeeService() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i3.Stream<List<_i4.Employee>> get employees => (super.noSuchMethod(
-        Invocation.getter(#employees),
-        returnValue: _i3.Stream<List<_i4.Employee>>.empty(),
-      ) as _i3.Stream<List<_i4.Employee>>);
-
-  @override
-  List<_i4.Employee> get allEmployees => (super.noSuchMethod(
-        Invocation.getter(#allEmployees),
-        returnValue: <_i4.Employee>[],
-      ) as List<_i4.Employee>);
-
-  @override
-  _i3.Stream<List<_i4.Employee>> get activeEmployees => (super.noSuchMethod(
-        Invocation.getter(#activeEmployees),
-        returnValue: _i3.Stream<List<_i4.Employee>>.empty(),
-      ) as _i3.Stream<List<_i4.Employee>>);
-
-  @override
-  _i3.Stream<_i4.Employee?> memberDetails(String? uid) => (super.noSuchMethod(
-        Invocation.method(
-          #memberDetails,
-          [uid],
+  _i2.FirebaseFirestore get fireStore => (super.noSuchMethod(
+        Invocation.getter(#fireStore),
+        returnValue: _FakeFirebaseFirestore_0(
+          this,
+          Invocation.getter(#fireStore),
         ),
-        returnValue: _i3.Stream<_i4.Employee?>.empty(),
-      ) as _i3.Stream<_i4.Employee?>);
+      ) as _i2.FirebaseFirestore);
 
   @override
-  _i3.Future<void> reset() => (super.noSuchMethod(
+  _i4.Stream<List<_i5.Employee>> employees(String? spaceId) =>
+      (super.noSuchMethod(
         Invocation.method(
-          #reset,
+          #employees,
+          [spaceId],
+        ),
+        returnValue: _i4.Stream<List<_i5.Employee>>.empty(),
+      ) as _i4.Stream<List<_i5.Employee>>);
+
+  @override
+  _i4.Future<void> addEmployeeBySpaceId({
+    required _i5.Employee? employee,
+    required String? spaceId,
+  }) =>
+      (super.noSuchMethod(
+        Invocation.method(
+          #addEmployeeBySpaceId,
+          [],
+          {
+            #employee: employee,
+            #spaceId: spaceId,
+          },
+        ),
+        returnValue: _i4.Future<void>.value(),
+        returnValueForMissingStub: _i4.Future<void>.value(),
+      ) as _i4.Future<void>);
+
+  @override
+  _i4.Future<_i5.Employee?> getEmployeeBySpaceId({
+    required String? userId,
+    required String? spaceId,
+  }) =>
+      (super.noSuchMethod(
+        Invocation.method(
+          #getEmployeeBySpaceId,
+          [],
+          {
+            #userId: userId,
+            #spaceId: spaceId,
+          },
+        ),
+        returnValue: _i4.Future<_i5.Employee?>.value(),
+      ) as _i4.Future<_i5.Employee?>);
+
+  @override
+  _i4.Future<List<_i5.Employee>> getEmployees() => (super.noSuchMethod(
+        Invocation.method(
+          #getEmployees,
           [],
         ),
-        returnValue: _i3.Future<void>.value(),
-        returnValueForMissingStub: _i3.Future<void>.value(),
-      ) as _i3.Future<void>);
+        returnValue: _i4.Future<List<_i5.Employee>>.value(<_i5.Employee>[]),
+      ) as _i4.Future<List<_i5.Employee>>);
 
   @override
-  _i3.Future<void> dispose() => (super.noSuchMethod(
+  _i4.Future<_i5.Employee?> getEmployee(String? id) => (super.noSuchMethod(
         Invocation.method(
-          #dispose,
-          [],
+          #getEmployee,
+          [id],
         ),
-        returnValue: _i3.Future<void>.value(),
-        returnValueForMissingStub: _i3.Future<void>.value(),
-      ) as _i3.Future<void>);
+        returnValue: _i4.Future<_i5.Employee?>.value(),
+      ) as _i4.Future<_i5.Employee?>);
+
+  @override
+  _i4.Future<bool> hasUser(String? email) => (super.noSuchMethod(
+        Invocation.method(
+          #hasUser,
+          [email],
+        ),
+        returnValue: _i4.Future<bool>.value(false),
+      ) as _i4.Future<bool>);
+
+  @override
+  _i4.Future<void> updateEmployeeDetails({required _i5.Employee? employee}) =>
+      (super.noSuchMethod(
+        Invocation.method(
+          #updateEmployeeDetails,
+          [],
+          {#employee: employee},
+        ),
+        returnValue: _i4.Future<void>.value(),
+        returnValueForMissingStub: _i4.Future<void>.value(),
+      ) as _i4.Future<void>);
+
+  @override
+  _i4.Future<void> changeAccountStatus({
+    required String? id,
+    required _i5.EmployeeStatus? status,
+  }) =>
+      (super.noSuchMethod(
+        Invocation.method(
+          #changeAccountStatus,
+          [],
+          {
+            #id: id,
+            #status: status,
+          },
+        ),
+        returnValue: _i4.Future<void>.value(),
+        returnValueForMissingStub: _i4.Future<void>.value(),
+      ) as _i4.Future<void>);
 }
