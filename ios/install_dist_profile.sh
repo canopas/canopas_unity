550d6b3e6bff8a1ddbad44db9a89a8d948db04cf
cp-sneha-s <sneha.s@canopas.com>
2024-03-23 22:10:22 +0530

    Minor changes

--- ios/install_dist_profile.sh
+++ ios/install_dist_profile.sh
@@ -8,7 +8,7 @@ echo $DIST_PROVISION | base64 --decode > $DIST_PROFILE_FILE
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 # copy where Xcode can find it
-cp ${DIST_PROFILE_FILE} "$HOME/Library/MobileDevice/Provisioning\ Profiles/${DIST_PROVISION_UUID}.mobileprovision"
+cp "${DIST_PROFILE_FILE}" "$HOME/Library/MobileDevice/Provisioning Profiles/${DIST_PROVISION_UUID}.mobileprovision"
 
 # clean
 rm -fr *.mobileprovision
