d1cc6abd374ca49e61a484e5955aaa13d76ceaa7
cp-sneha-s <sneha.s@canopas.com>
2024-03-23 21:59:45 +0530

    Minor changes

--- ios/install_dist_profile.sh
+++ ios/install_dist_profile.sh
@@ -3,12 +3,12 @@
 DIST_PROFILE_FILE=${DIST_PROVISION_UUID}.mobileprovision
 
 # Recreate the certificate from the secure environment variable
-echo -n "$BUILD_PROVISION_PROFILE_BASE64" | base64 --decode -o > $DIST_PROFILE_FILE
+echo $DIST_PROVISION | base64 --decode > $DIST_PROFILE_FILE
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 # copy where Xcode can find it
-cp ${DIST_PROFILE_FILE} "$HOME/Library/MobileDevice/Provisioning Profiles/${DIST_PROVISION_UUID}.mobileprovision"
+cp ${DIST_PROFILE_FILE} "$HOME/Library/MobileDevice/Provisioning\ Profiles/${DIST_PROVISION_UUID}.mobileprovision"
 
 # clean
 rm -fr *.mobileprovision
