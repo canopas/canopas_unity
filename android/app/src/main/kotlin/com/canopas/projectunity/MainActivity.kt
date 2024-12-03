30f563367fe374c135a0d9d82096f20a7e78992d
Sneha Canopas <sneha.s@canopas.com>
2024-12-03 16:04:32 +0530

    Fix android build

--- android/app/src/main/kotlin/com/canopas/projectunity/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.canopas.projectunity
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/canopas/unity/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.canopas.unity
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
