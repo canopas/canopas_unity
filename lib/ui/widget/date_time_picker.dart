929667a147000b047ee234eecb369e29d0575920
Sneha Canopas <92501869+cp-sneha-s@users.noreply.github.com>
2024-12-03 12:24:03 +0530

    Minor fixes (#104)
    * Fix android pipeline
    
    * Fix lint error

--- lib/ui/widget/date_time_picker.dart
+++ lib/ui/widget/date_time_picker.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:projectunity/data/core/extensions/context_extension.dart';
 import 'package:projectunity/data/core/extensions/date_time.dart';
 
 Future<DateTime?> pickDate(
     {required BuildContext context, required DateTime initialDate}) async {
   DateTime? pickDate = await showDatePicker(
     context: context,
-    barrierColor: context.colorScheme.surface,
     initialDate: initialDate,
     firstDate: DateTime(1990),
     lastDate: DateTime(DateTime.now().futureDateSelectionYear),
@@ -19,7 +17,6 @@ Future<TimeOfDay?> pickTime(
   TimeOfDay? time = await showTimePicker(
     context: context,
     initialTime: initialTime,
-    barrierColor: context.colorScheme.surface,
   );
   return time;
 }
